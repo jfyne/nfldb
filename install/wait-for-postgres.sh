0d33ac8aa569dc2339f4d77edcd1250af7404514
Josh Fyne <josh.fyne@gmail.com>
2017-01-16 09:24:35 -0500

    Cleanup

--- install/wait-for-postgres.sh
+++ install/wait-for-postgres.sh
@@ -7,10 +7,6 @@ host="$1"
 shift
 cmd="$@"
 
-#until psql -h "$host" -U "postgres" -c '\l'; do
-#  >&2 echo "Postgres is unavailable - sleeping"
-#  sleep 1
-#done
 until echo exit | nc -v "$host" 5432; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
